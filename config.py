53f81cdd364becad86a17ebdc7fb300bcad78aa1
poochasanyasi5326-ops <poochasanyasi5326@gmail.com>
2026-01-26 15:40:09 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+OWNER_ID = 519459195
+CHANNEL_ID = -1003527330797
+CHANNEL_LINK = "https://t.me/+eooytvOAwjc0NTI1"
+API_ID = int(os.getenv("API_ID", "0"))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+DOWNLOAD_DIR = "/tmp/downloads"
+DB_PATH = "/tmp/bot_db.json"
+WATERMARK = "@poocha" # Change this to your fixed watermark text
